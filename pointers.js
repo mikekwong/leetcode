05606908d882dac288a6167e3260ffbf67b24fa0
Mike Wong <wongmdesign@gmail.com>
2020-01-06 16:25:53 -0500

    merged arrays added

--- pointers.js
+++ pointers.js
@@ -1,3 +1,30 @@
+// time - O(n + m)
+function mergeArrays (arr1, arr2) {
+  let merged = []
+  let i = 0
+  let j = 0
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      merged.push(arr1[i])
+      i++
+    } else {
+      merged.push(arr2[j])
+      j++
+    }
+  }
+  // If an array is completely traversed, while other one is left then just copy all the remaining elements into result array
+  if (i <= arr1.length - 1) {
+    arr1.splice(0, i)
+    merged = merged.concat(arr1)
+  } else if (j <= arr2.length - 1) {
+    arr2.splice(0, j)
+    merged = merged.concat(arr2)
+  }
+
+  return merged
+}
+
 function removeDuplicates (arr) {
   // index of next non-duplicate item
   let left = 1
